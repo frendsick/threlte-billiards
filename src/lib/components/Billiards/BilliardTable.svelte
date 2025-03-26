67fd5ebac6cf5956f98fd349294e32a232e2a436
Teemu Pätsi <frendsick@gmail.com>
2025-03-26 21:20:33 +0200

    feat: Crude BilliardTable component

---
+++ src/lib/components/Billiards/BilliardTable.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+    import * as THREE from "three";
+    import { T } from "@threlte/core";
+    import { AutoColliders } from "@threlte/rapier";
+    import { CSG } from "three-csg-ts";
+
+    interface Props {
+        position: [number, number, number];
+    }
+    let { position }: Props = $props();
+
+    // https://www.dimensions.com/element/9-foot-billiards-pool-table
+    const TABLE_LENGTH = 290;
+    const TABLE_WIDTH = 163;
+    const TABLE_HEIGHT = 81.3;
+
+    // https://www.dimensions.com/element/billiard-balls
+    const BALL_DIAMETER = 5.715;
+    const BALL_RADIUS = BALL_DIAMETER / 2;
+
+    const PLAYING_AREA_LENGTH = 254;
+    const PLAYING_AREA_WIDTH = 127;
+
+    const APRON_HEIGHT = 30;
+    const PLAYING_AREA_DEPTH = BALL_RADIUS + 3;
+
+    const TABLE_LEG_HEIGHT = TABLE_HEIGHT - APRON_HEIGHT;
+    const TABLE_LEG_WIDTH = 20;
+
+    const TABLE_LEG_MESH_POSITIONS = [
+        [
+            TABLE_WIDTH / 2 - TABLE_LEG_WIDTH / 2,
+            -APRON_HEIGHT / 2 - TABLE_LEG_HEIGHT / 2,
+            TABLE_LENGTH / 2 - TABLE_LEG_WIDTH / 2,
+        ],
+        [
+            -TABLE_WIDTH / 2 + TABLE_LEG_WIDTH / 2,
+            -APRON_HEIGHT / 2 - TABLE_LEG_HEIGHT / 2,
+            TABLE_LENGTH / 2 - TABLE_LEG_WIDTH / 2,
+        ],
+        [
+            -TABLE_WIDTH / 2 + TABLE_LEG_WIDTH / 2,
+            -APRON_HEIGHT / 2 - TABLE_LEG_HEIGHT / 2,
+            -TABLE_LENGTH / 2 + TABLE_LEG_WIDTH / 2,
+        ],
+        [
+            TABLE_WIDTH / 2 - TABLE_LEG_WIDTH / 2,
+            -APRON_HEIGHT / 2 - TABLE_LEG_HEIGHT / 2,
+            -TABLE_LENGTH / 2 + TABLE_LEG_WIDTH / 2,
+        ],
+    ] as const;
+
+    function tableLegMesh(x: number, y: number, z: number) {
+        const tableLeg = new THREE.Mesh(
+            new THREE.BoxGeometry(TABLE_LEG_WIDTH, TABLE_LEG_HEIGHT, TABLE_LEG_WIDTH),
+        );
+        tableLeg.position.set(x, y, z);
+        tableLeg.updateMatrix();
+        return tableLeg;
+    }
+
+    function tableMesh() {
+        // Construct apron without playing area
+        const apron = new THREE.Mesh(
+            new THREE.BoxGeometry(TABLE_WIDTH, APRON_HEIGHT, TABLE_LENGTH),
+        );
+        apron.updateMatrix();
+
+        // Cut playing area from apron
+        const playingArea = new THREE.Mesh(
+            new THREE.BoxGeometry(PLAYING_AREA_WIDTH, PLAYING_AREA_DEPTH, PLAYING_AREA_LENGTH),
+        );
+        playingArea.position.set(0, APRON_HEIGHT / 2 - PLAYING_AREA_DEPTH / 2, 0);
+        playingArea.updateMatrix();
+        const table = CSG.subtract(apron, playingArea);
+
+        // Add legs to the table
+        const tableWithLegs = TABLE_LEG_MESH_POSITIONS.reduce((table, position) => {
+            return CSG.union(table, tableLegMesh(...position));
+        }, table);
+
+        return tableWithLegs;
+    }
+
+    const table = tableMesh();
+</script>
+
+<T.DirectionalLight
+    position={[5, 10, 5]}
+    intensity={1}
+    castShadow={true}
+    shadow-mapSize-width={1024}
+    shadow-mapSize-height={1024}
+/>
+
+<T.PointLight position={[0, 10, 0]} intensity={1} castShadow={true} />
+
+<T.Group {position}>
+    <AutoColliders shape="cuboid">
+        <T.Mesh
+            receiveShadow
+            geometry={table.geometry}
+            material={new THREE.MeshStandardMaterial()}
+        />
+    </AutoColliders>
+</T.Group>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,25 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { Canvas, T } from "@threlte/core";
+    import { World } from "@threlte/rapier";
+    import BilliardTable from "$lib/components/Billiards/BilliardTable.svelte";
+    import { OrbitControls } from "@threlte/extras";
+</script>
+
+<div>
+    <Canvas>
+        <World>
+            <T.PerspectiveCamera makeDefault position.x={300} position.y={200}>
+                <OrbitControls target.x={25} />
+            </T.PerspectiveCamera>
+            <T.DirectionalLight castShadow position={[8, 20, -3]} />
+
+            <BilliardTable position={[0, 0, 0]} />
+        </World>
+    </Canvas>
+</div>
+
+<style>
+    div {
+        height: 100%;
+    }
+</style>
